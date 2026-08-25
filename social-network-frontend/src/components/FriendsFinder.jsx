5f8ce5ce8ca747f2fae39cecddf52c580d0fd170
kiranchitram <165831369+kiranchitram@users.noreply.github.com>
2026-08-25 18:58:10 +0530

    Update FriendsFinder.jsx

--- social-network-frontend/src/components/FriendsFinder.jsx
+++ social-network-frontend/src/components/FriendsFinder.jsx
@@ -15,7 +15,7 @@ function FriendsFinder() {
     setLoading(true);
     setError("");
     try {
-      const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/users/Alice/friends`);
+      const res = await fetch("https://social-network-backend.onrender.com/users/Alice/friends")
       if (!res.ok) throw new Error("Failed to fetch");
       const data = await res.json();
       setFriends(data);
